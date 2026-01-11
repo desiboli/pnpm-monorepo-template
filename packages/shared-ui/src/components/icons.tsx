37a5ce7e454536e5217b38547c65627925a5c255
desiboli <adam.benals@gmail.com>
2026-01-11 09:40:37 +0100

    shared ui package

---
+++ packages/shared-ui/src/components/icons.tsx
@@ -0,0 +1,18 @@
+import {
+	CheckIcon,
+	ChevronRightIcon,
+	CircleIcon,
+	type LucideIcon,
+	Moon,
+	Sun,
+} from "lucide-react";
+
+export type Icon = LucideIcon | typeof CheckIcon;
+
+export const Icons = {
+	check: CheckIcon,
+	chevronRight: ChevronRightIcon,
+	circle: CircleIcon,
+	moon: Moon,
+	sun: Sun,
+};
---
+++ packages/shared-ui/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./components/icons";
+export * from "./components/ui/button";
+export * from "./components/ui/card";
+export * from "./components/ui/dropdown-menu";
+export * from "./lib/utils";
